9a959b8db82cd4ab593f47cf998d8dbc258ba612
newt0 <py_lambda@yahoo.co.jp>
2020-12-07 10:00:09 +0900

    add: action buttons in Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,51 @@
 import React from "react";
 import "./Footer.css";
-
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import { Grid, Slider } from "@material-ui/core";
 const Footer = () => {
-  return <div className="footer">footer</div>;
+  return (
+    <div className="footer">
+      <div className="footer__left">
+        <img
+          className="footer__albumLogo"
+          src="https://linkstorage.linkfire.com/medialinks/images/56fcac46-e886-49b8-9bfe-09b3ddae8665/artwork-440x440.jpg"
+          alt=""
+        />
+        <div className="footer__songInfo">
+          <h4>BUMP OF CHICKEN</h4>
+          <p>...</p>
+        </div>
+      </div>
+
+      <div className="footer__center">
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
+      </div>
+
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider aria-labelledby="continuous-slider" />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
